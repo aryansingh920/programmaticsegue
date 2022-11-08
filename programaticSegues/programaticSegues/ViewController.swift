154dd4c817820c7828fcce4860fcd49e135b2fd8
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2022-11-08 11:59:16 +0530

    Add files via upload

---
+++ programaticSegues/programaticSegues/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  programaticSegues
+//
+//  Created by Aryan on 08/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func yellowButtonTap(_ sender: Any) {
+        if(segueSwitch.isOn){
+            performSegue(withIdentifier: "yellow", sender: nil)
+        }
+    }
+    
+
+    @IBAction func greenButtonTap(_ sender: Any)
+    {
+        if(segueSwitch.isOn){
+            performSegue(withIdentifier: "green", sender: nil)
+        }
+    }
+    
+}
+
